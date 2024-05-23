0e609a5c9fa15decd21b50f385e0a0ddcf4a89d3
ethan <epollack31@gmail.com>
2024-05-22 18:21:36 -0700

    added basic game logic with Game and Time classes and useGame hook

---
+++ src/components/GameCanvas.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { Game } from "@/game/Game";
+import { useUpdateGame } from "@/game/useGame";
+import { useEffect, useRef, useState } from "react";
+
+export default function GameCanvas() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [gameInstance, setGameInstance] = useState<Game | null>(null);
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      const game = new Game(canvasRef.current);
+      setGameInstance(game);
+    }
+  }, []);
+
+  useUpdateGame(gameInstance);
+
+  return (
+    <div>
+      <canvas ref={canvasRef} id="game" width="800" height="600"></canvas>
+    </div>
+  );
+}

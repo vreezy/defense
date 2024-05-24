4588e2172c18f1ecdedb823536afe3c4e05de493
ethan <epollack31@gmail.com>
2024-05-24 15:13:13 -0700

    added GreenUFOModel instead of sphere mesh

---
+++ src/components/game/models/GreenUFOModel.tsx
@@ -0,0 +1,47 @@
+import * as THREE from "three";
+import React, { useRef } from "react";
+import { useGLTF } from "@react-three/drei";
+import { GLTF } from "three-stdlib";
+
+type GLTFResult = GLTF & {
+  nodes: {
+    Mesh_enemy_ufoGreen: THREE.Mesh;
+    Mesh_enemy_ufoGreen_1: THREE.Mesh;
+    Mesh_enemy_ufoGreen_2: THREE.Mesh;
+  };
+  materials: {
+    snowRoad: THREE.MeshStandardMaterial;
+    glass: THREE.MeshStandardMaterial;
+    foliage: THREE.MeshStandardMaterial;
+  };
+};
+
+export default function Model(props: JSX.IntrinsicElements["group"]) {
+  const { nodes, materials } = useGLTF(
+    "/models/enemy_ufoGreen.glb"
+  ) as GLTFResult;
+  return (
+    <group {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Mesh_enemy_ufoGreen.geometry}
+        material={materials.snowRoad}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Mesh_enemy_ufoGreen_1.geometry}
+        material={materials.glass}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Mesh_enemy_ufoGreen_2.geometry}
+        material={materials.foliage}
+      />
+    </group>
+  );
+}
+
+useGLTF.preload("/models/enemy_ufoGreen.glb");

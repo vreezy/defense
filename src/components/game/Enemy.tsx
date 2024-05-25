f77715713563d361393aeb25dabe7d5fb4f9fe85
ethan <epollack31@gmail.com>
2024-05-25 16:33:43 -0700

    moved enemy updating to Enemy

--- src/components/game/Enemy.tsx
+++ src/components/game/Enemy.tsx
@@ -1,17 +1,20 @@
 import React, { useEffect, useMemo } from "react";
-import { MeshProps } from "@react-three/fiber";
+import { MeshProps, useFrame } from "@react-three/fiber";
 import GreenUFOModel from "./models/GreenUFOModel";
 import { a, useSpring } from "@react-spring/three";
 import usePulse from "@/game/utils/usePulse";
 import { type Enemy as EnemyType } from "@/game/types";
 import { useGameStore } from "@/game/store";
+import { getNextDirection } from "@/game/utils/pathfinding";
+import { positionEquals } from "@/game/utils/positionEquals";
 
 interface EnemyProps extends Omit<MeshProps, "position"> {
   enemy: EnemyType;
 }
 
 export const Enemy: React.FC<EnemyProps> = ({ enemy }) => {
-  const despawnEnemy = useGameStore((state) => state.despawnEnemy);
+  const { despawnEnemy, grid, weapons, removeEnemy, updateEnemy } =
+    useGameStore((state) => state);
   const offset = useMemo(() => Math.random() * Math.PI, []);
   const height = usePulse(0.1, 0.2, 2, offset);
 
@@ -32,6 +35,31 @@ export const Enemy: React.FC<EnemyProps> = ({ enemy }) => {
       despawnEnemy(enemy.id);
     }
   }, [enemy.id, enemy.removed, scale]);
+
+  const obstacles = useMemo(() => {
+    return weapons.map((weapon) => ({
+      position: weapon.position,
+      radius: weapon.radius,
+    }));
+  }, [weapons]);
+
+  useFrame(() => {
+    const direction = getNextDirection(enemy, grid, obstacles);
+    const position: [number, number] = [
+      enemy.position[0] + Math.cos(direction) * enemy.speed,
+      enemy.position[1] + Math.sin(direction) * enemy.speed,
+    ];
+
+    if (positionEquals(position, grid.end)) {
+      removeEnemy(enemy.id, "fly");
+    } else {
+      updateEnemy({
+        ...enemy,
+        position,
+        direction,
+      });
+    }
+  });
 
   return (
     <a.group
--- src/components/game/EnemyHandler.tsx
+++ src/components/game/EnemyHandler.tsx
@@ -2,47 +2,14 @@
 
 import { useGameStore } from "@/game/store";
 import { Enemy } from "./Enemy";
-import useIntervalFrame from "@/game/utils/useIntervalFrame";
-import { useFrame } from "@react-three/fiber";
-import { getNextDirection } from "@/game/utils/pathfinding";
-import { positionEquals } from "@/game/utils/positionEquals";
-import { useMemo } from "react";
 import { useInterval } from "react-use";
 
 export default function EnemyHandler() {
-  const { grid, enemies, weapons, spawnEnemy, updateEnemy, removeEnemy } =
-    useGameStore((state) => state);
-
-  const obstacles = useMemo(() => {
-    return weapons.map((weapon) => ({
-      position: weapon.position,
-      radius: weapon.radius,
-    }));
-  }, [weapons]);
+  const { grid, enemies, spawnEnemy } = useGameStore((state) => state);
 
   useInterval(() => {
     spawnEnemy(grid.start);
   }, 1000);
-
-  useFrame(() => {
-    enemies.forEach((enemy, i) => {
-      const direction = getNextDirection(enemy, grid, obstacles);
-      const position: [number, number] = [
-        enemy.position[0] + Math.cos(direction) * enemy.speed,
-        enemy.position[1] + Math.sin(direction) * enemy.speed,
-      ];
-
-      if (positionEquals(position, grid.end)) {
-        removeEnemy(enemy.id, "fly");
-      } else {
-        updateEnemy({
-          ...enemy,
-          position,
-          direction,
-        });
-      }
-    });
-  });
 
   return enemies.map((enemy) => <Enemy key={enemy.id} enemy={enemy} />);
 }

e326e5be4362c5b41394b2aef45ba3165c58d946
ethan <epollack31@gmail.com>
2024-05-26 16:39:02 -0700

    moved enemy updating logic to EnemyHandler

--- src/components/game/Enemy.tsx
+++ src/components/game/Enemy.tsx
@@ -1,20 +1,15 @@
-import React, { useEffect, useMemo } from "react";
-import { MeshProps, useFrame } from "@react-three/fiber";
+import React, { useMemo } from "react";
+import { MeshProps } from "@react-three/fiber";
 import GreenUFOModel from "./models/GreenUFOModel";
 import { a, useSpring } from "@react-spring/three";
 import usePulse from "@/game/utils/usePulse";
 import { type Enemy as EnemyType } from "@/game/types";
-import { useGameStore } from "@/game/store";
-import { getNextDirection } from "@/game/utils/pathfinding";
-import { positionEquals } from "@/game/utils/positionEquals";
 
 interface EnemyProps extends Omit<MeshProps, "position"> {
   enemy: EnemyType;
 }
 
 export const Enemy: React.FC<EnemyProps> = ({ enemy }) => {
-  const { despawnEnemy, grid, weapons, removeEnemy, updateEnemy } =
-    useGameStore((state) => state);
   const offset = useMemo(() => Math.random() * Math.PI, []);
   const height = usePulse(0.1, 0.2, 2, offset);
 
@@ -28,37 +23,6 @@ export const Enemy: React.FC<EnemyProps> = ({ enemy }) => {
     config: { mass: 1, tension: 100, friction: 20 },
     delay: 0,
     loop: false,
-  });
-
-  useEffect(() => {
-    if (enemy.removed && scale.get() < 0.1) {
-      despawnEnemy(enemy.id);
-    }
-  }, [enemy.id, enemy.removed, scale]);
-
-  const obstacles = useMemo(() => {
-    return weapons.map((weapon) => ({
-      position: weapon.position,
-      radius: weapon.radius,
-    }));
-  }, [weapons]);
-
-  useFrame(() => {
-    const direction = getNextDirection(enemy, grid, obstacles);
-    const position: [number, number] = [
-      enemy.position[0] + Math.cos(direction) * enemy.speed,
-      enemy.position[1] + Math.sin(direction) * enemy.speed,
-    ];
-
-    if (positionEquals(position, grid.end)) {
-      removeEnemy(enemy.id, "fly");
-    } else {
-      updateEnemy({
-        ...enemy,
-        position,
-        direction,
-      });
-    }
   });
 
   return (
--- src/components/game/EnemyHandler.tsx
+++ src/components/game/EnemyHandler.tsx
@@ -1,15 +1,59 @@
 "use client";
 
 import { useGameStore } from "@/game/store";
-import { Enemy } from "./Enemy";
+import { Enemy as EnemyModel } from "./Enemy";
 import { useInterval } from "react-use";
+import { useCallback, useMemo } from "react";
+import { useFrame } from "@react-three/fiber";
+import { getNextDirection } from "@/game/utils/pathfinding";
+import { positionEquals } from "@/game/utils/positionEquals";
+import { Enemy } from "@/game/types";
 
 export default function EnemyHandler() {
-  const { grid, enemies, spawnEnemy } = useGameStore((state) => state);
+  const { grid, enemies, spawnEnemy, updateEnemy, removeEnemy, despawnEnemy, weapons, setSelectedWeapon } = useGameStore((state) => state);
 
   useInterval(() => {
     spawnEnemy(grid.start);
   }, 1000);
 
-  return enemies.map((enemy) => <Enemy key={enemy.id} enemy={enemy} />);
+  const obstacles = useMemo(() => {
+    return weapons.map((weapon) => ({
+      position: weapon.position,
+      radius: weapon.radius,
+    }));
+  }, [weapons]);
+
+  const handleEnemyUpdate = useCallback((enemy: Enemy, delta: number) => {
+    // console.log("delta", delta)
+    const direction = getNextDirection(enemy, grid, obstacles);
+    const speed = enemy.speed * delta * 100; // Adjust speed based on delta time
+    const position: [number, number] = [
+      enemy.position[0] + Math.cos(direction) * speed,
+      enemy.position[1] + Math.sin(direction) * speed,
+    ];
+  
+    if (positionEquals(position, grid.end)) {
+      if (enemy.removed !== "fly") {
+        removeEnemy(enemy.id, "fly");
+        setTimeout(() => {
+          despawnEnemy(enemy.id);
+        }, 2000);
+      }
+    } else {
+      updateEnemy({
+        ...enemy,
+        position,
+        direction,
+      });
+    }
+}, []); 
+
+  useFrame((_, delta) => {
+    enemies.forEach((enemy) => {
+      handleEnemyUpdate(enemy, delta);
+    });
+  });
+  
+
+  return enemies.map((enemy) => <EnemyModel key={enemy.id} enemy={enemy} />);
 }

1e292ea56ffc5828b622a18b0ee7e7138f54245e
ethan <epollack31@gmail.com>
2024-05-27 20:05:34 -0700

    better weapon spawning (different radii, speeds, damages, and focus modes)

--- src/game/store.ts
+++ src/game/store.ts
@@ -1,5 +1,12 @@
 import { create } from "zustand";
-import { Enemy, EnemyRemoveType, Grid, Weapon, WeaponType } from "./types";
+import {
+  Enemy,
+  EnemyRemoveType,
+  Grid,
+  Weapon,
+  WeaponFocusMode,
+  WeaponType,
+} from "./types";
 import { getAngle } from "./utils/getAngle";
 
 export interface GameState {
@@ -24,6 +31,41 @@ export interface GameActions {
 }
 export type GameStore = GameState & GameActions;
 
+function newWeapon(state: GameState, position: [number, number]): Weapon {
+  if (state.weaponSpawnType === null) {
+    throw new Error("Weapon spawn type is null");
+  }
+
+  let properties: Partial<Weapon> = {};
+
+  switch (state.weaponSpawnType) {
+    case "Turret":
+      properties = {
+        radius: 2,
+        focusMode: "nearest",
+        speed: 0.1,
+        damage: 0.1,
+      };
+      break;
+    case "Cannon":
+      properties = {
+        radius: 3,
+        focusMode: "strongest",
+        speed: 0.5,
+        damage: 0.5,
+      };
+      break;
+  }
+
+  const now = Date.now();
+  return {
+    id: now.toString(),
+    position,
+    type: state.weaponSpawnType!,
+    ...properties,
+  } as Weapon;
+}
+
 export const useGameStore = create<GameStore>()((set) => ({
   grid: {
     rows: 10,
@@ -43,8 +85,8 @@ export const useGameStore = create<GameStore>()((set) => ({
         {
           id: Date.now().toString(),
           position: [state.grid.start[0], 10, state.grid.start[1]],
-          maxHealth: 20,
-          health: 20,
+          maxHealth: 10,
+          health: 10,
           speed: 0.01,
           direction: getAngle(state.grid.start, state.grid.end),
           spawnedAt: Date.now(),
@@ -72,19 +114,7 @@ export const useGameStore = create<GameStore>()((set) => ({
 
   spawnWeapon: (position: [number, number]) =>
     set((state) => ({
-      weapons: [
-        ...state.weapons,
-        {
-          id: Date.now().toString(),
-          spawnTime: Date.now(),
-          position,
-          radius: 2,
-          focusMode: "nearest",
-          speed: 0.1,
-          damage: 0.1,
-          type: state.weaponSpawnType!,
-        },
-      ],
+      weapons: [...state.weapons, newWeapon(state, position)],
     })),
   updateWeapon: (update: Weapon) =>
     set((state) => ({
